1552f3fa156554ebf401822f40cb74e663a10bad
MiaoPt <mickyzhang0817@gmail.com>
2026-06-14 11:42:26 +0100

    Update to headless

--- scrape-script.js
+++ scrape-script.js
@@ -32,7 +32,7 @@ function log(message, level = 'INFO') {
 //Scrape function
 (async function scrapeFreecycle () {
     const browser = await chromium.launch({
-        headless: false,
+        headless: true,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
     try{
